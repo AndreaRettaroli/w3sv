783638e94814f3b406944acc79c346c9bf2fd70b
Andrea Rettaroli <andrea.rettaroli@studio.unibo.it>
2024-05-17 20:24:31 +0200

    feat: :lipstick: customized cursor

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import { useEffect } from "react";
 import ActionSelect from "./components/ActionSelect";
+import { CursorEffectResult, trailingCursor } from "cursor-effects";
 
 export default function Home() {
+  useEffect(() => {
+    const effect: CursorEffectResult = trailingCursor();
+    return () => {
+      effect.destroy();
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="bg-black text-green-500 font-mono flex flex-col justify-end p-4">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 cursor-none">
+      <div className="bg-black text-green-500 font-mono flex flex-col justify-end p-4 text-2xl">
         <code className="animate-typing overflow-hidden whitespace-nowrap">
           <p>
             $ Welcome Web3 Seed Vault
